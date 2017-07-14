cd510e369183569d59bc596e8a26e0c814852182
项罗阳 <chaosx@yeah.net>
2017-07-14 17:48:34 +0800

    注册登录·

--- blog/src/components/Users/register.jsx
+++ blog/src/components/Users/register.jsx
@@ -7,6 +7,7 @@ class Register extends React.Component {
             <div className="root">
                 <div className="typeInPlate">
                     <ul className="sign-up-container">
+                        <li className="urls">注册·登录</li>
                         <li className="lis"><input className="inputs sign-up-username" placeholder="你的昵称" type="text" /></li>
                         <li className="lis"><input className="inputs sign-up-passwd" placeholder="设置密码" type="password" /></li>
                         {/*绑定邮箱以后可以加入邮箱验证系统  */}
