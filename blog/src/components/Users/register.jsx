a78bcf984dd7021a3c2cf13745a25189b825d7b3
项罗阳 <chaosx@yeah.net>
2017-07-15 21:01:52 +0800

    insert i

--- blog/src/components/Users/register.jsx
+++ blog/src/components/Users/register.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './user.css'
-
+import '../../static/css/iconfont.css'
 class Register extends React.Component {
     render() {
         return (
@@ -8,10 +8,25 @@ class Register extends React.Component {
                 <div className="typeInPlate">
                     <ul className="sign-up-container">
                         <li className="urls">注册·登录</li>
-                        <li className="lis"><input className="inputs sign-up-username" placeholder="你的昵称" type="text" /></li>
-                        <li className="lis"><input className="inputs sign-up-passwd" placeholder="设置密码" type="password" /></li>
+                        <li className="lis">
+                            <div className="board">
+                                <i className="iconfont icon-0055"></i>
+                                <input className="inputs sign-up-username" placeholder="你的昵称" type="text" />
+                            </div>    
+                        </li>
+                        <li className="lis">
+                            <div className="board">
+                                <i className="iconfont icon-mima"></i>
+                                <input className="inputs sign-up-passwd" placeholder="设置密码" type="password" />
+                            </div>    
+                        </li>
                         {/*绑定邮箱以后可以加入邮箱验证系统  */}
-                        <li className="lis"><input className="inputs sign-up-email" placeholder="绑定邮箱" type="text" /></li>
+                        <li className="lis">
+                            <div className="board">
+                                <i className="iconfont icon-youxiang"></i>
+                                <input className="inputs sign-up-email" placeholder="绑定邮箱" type="text" />
+                            </div>
+                        </li>
                         <li className="lis"><button className="submit">注册</button></li>
                         <p className="sign-up-msg">点击 “注册” 即表示您同意并愿意遵守简书用户协议 和 隐私政策 。</p>
                     </ul>
